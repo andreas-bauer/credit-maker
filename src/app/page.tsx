a4f3bc06aa2cf595750bf77bbef4a84a7b82676f
Andreas Bauer <dev@andreasbauer.org>
2024-07-09 00:32:51 +0200

    Refactor Credit types

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,62 +1,81 @@
+'use client'
 import { allCreditRoles } from '../lib/credit-roles'
 
 export default function Home() {
+  const generateCredit = (formData: FormData) => {
+    console.log(formData.get('author-name'))
+  }
+
   return (
     <main className='flex min-h-screen flex-col items-center justify-between p-12'>
       <div className='flex w-full flex-row gap-4'>
         {/* Left side */}
         <div className='w-1/2 divide-y divide-gray-200 overflow-hidden rounded-lg bg-white shadow'>
-          <div className='px-4 py-5'>
+          <div className='flex flex-wrap items-center justify-between px-4 py-5'>
             {/* Card header */}
             <label className='text-xl'>Author information</label>
-          </div>
-          <div className='space-y-4 p-5'>
-            {/* Content goes here */}
-            <div className='rounded-md p-3 shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-indigo-600'>
-              <label
-                htmlFor='author-name'
-                className='block text-xs font-medium text-gray-900'
-              >
-                Author name
-              </label>
-              <input
-                id='author-name'
-                name='author-name'
-                type='text'
-                placeholder='Andreas Bauer'
-                className='block w-full border-0 p-0 text-gray-900 placeholder:text-gray-400 focus:ring-0'
-              />
-            </div>
 
-            <fieldset>
-              <legend className='sr-only'>Credit roles</legend>
-              <div className='space-y-5'>
-                {allCreditRoles.map((role) => (
-                  <div key={role.name} className='relative flex items-start'>
-                    <div className='flex h-6 items-center'>
-                      <input
-                        id={`ckb-${role.name}`}
-                        name={role.name}
-                        type='checkbox'
-                        aria-describedby={`${role.name}-description`}
-                        className='h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-600'
-                      />
-                    </div>
-                    <div className='ml-3 text-sm leading-6'>
-                      <label
-                        htmlFor={`ckb-${role.name}`}
-                        className='font-medium text-gray-900'
-                      >
-                        {role.name}
-                      </label>
-                      <p id={`ckb-${role.name}`} className='text-gray-500'>
-                        {role.definition}
-                      </p>
-                    </div>
-                  </div>
-                ))}
+            <button
+              type='submit'
+              form='author-form'
+              className='relative inline-flex items-center rounded-md bg-indigo-500 px-2 py-1 text-sm font-semibold text-white shadow-sm hover:bg-indigo-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500'
+            >
+              Generate
+            </button>
+          </div>
+          <div className='p-5'>
+            {/* Content goes here */}
+            <form
+              id='author-form'
+              className='space-y-4 '
+              action={generateCredit}
+            >
+              <div className='rounded-md p-3 shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-indigo-600'>
+                <label
+                  htmlFor='author-name'
+                  className='block text-xs font-medium text-gray-900'
+                >
+                  Author name
+                </label>
+                <input
+                  id='author-name'
+                  name='author-name'
+                  type='text'
+                  placeholder='Andreas Bauer'
+                  className='block w-full border-0 p-0 text-gray-900 placeholder:text-gray-400 focus:ring-0'
+                />
               </div>
-            </fieldset>
+
+              <fieldset>
+                <legend className='sr-only'>Credit roles</legend>
+                <div className='space-y-5'>
+                  {Object.entries(allCreditRoles).map(([key, role]) => (
+                    <div key={key} className='relative flex items-start'>
+                      <div className='flex h-6 items-center'>
+                        <input
+                          id={key}
+                          name={role.name}
+                          type='checkbox'
+                          aria-describedby={`${key}-description`}
+                          className='h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-600'
+                        />
+                      </div>
+                      <div className='ml-3 text-sm leading-6'>
+                        <label
+                          htmlFor={`ckb-${role.name}`}
+                          className='font-medium text-gray-900'
+                        >
+                          {role.name}
+                        </label>
+                        <p id={`ckb-${role.name}`} className='text-gray-500'>
+                          {role.definition}
+                        </p>
+                      </div>
+                    </div>
+                  ))}
+                </div>
+              </fieldset>
+            </form>
           </div>
         </div>
 
--- src/lib/credit-roles.ts
+++ src/lib/credit-roles.ts
@@ -1,92 +1,91 @@
-export interface Credit {
-  conceptualization: boolean
-  dataCuration: boolean
-  formalAnalysis: boolean
-  funding: boolean
-  investigation: boolean
-  methodology: boolean
-  projectManagement: boolean
-  resource: boolean
-  software: boolean
-  supervision: boolean
-  validation: boolean
-  visualization: boolean
-  writing: boolean
-  writingReview: boolean
-}
+export type Credit =
+  | 'conceptualization'
+  | 'dataCuration'
+  | 'formalanAlysis'
+  | 'funding'
+  | 'investigation'
+  | 'methodology'
+  | 'projectManagement'
+  | 'resource'
+  | 'software'
+  | 'supervision'
+  | 'validation'
+  | 'visualization'
+  | 'writing'
+  | 'writingReview'
 
 export interface CreditRole {
   name: string
   definition: string
 }
 
-export const allCreditRoles: CreditRole[] = [
-  {
+export const allCreditRoles: Record<Credit, CreditRole> = {
+  conceptualization: {
     name: 'Conceptualization',
     definition:
       'Ideas, formulation or evolution of overarching research goals and aims.',
   },
-  {
+  dataCuration: {
     name: 'Data curation',
     definition:
       'Produce metadata, scrub data and maintain research data for initial use and later re-use.',
   },
-  {
+  formalanAlysis: {
     name: 'Formal analysis',
     definition:
       'Application of statistical, mathematical, computational, or other formal techniques to analyze data.',
   },
-  {
+  funding: {
     name: 'Funding acquisition',
     definition:
       'Acquisition of the financial support for the project leading to this publication.',
   },
-  {
+  investigation: {
     name: 'Investigation',
     definition:
       'Conducting a research and investigation process, specifically performing the data collection.',
   },
-  {
+  methodology: {
     name: 'Methodology',
     definition: 'Development or design of methodology; creation of models.',
   },
-  {
+  projectManagement: {
     name: 'Project Management',
     definition:
       'Management and coordination responsibility for the research activity planning and execution.',
   },
-  {
+  resource: {
     name: 'Resources',
     definition:
       'Provision of study materials, instrumentation, computing resources, or other analysis tools.',
   },
-  {
+  software: {
     name: 'Software',
     definition:
       'Programming, software development; designing computer programs; implementation of the computer code and supporting algorithms.',
   },
-  {
+  supervision: {
     name: 'Supervision',
     definition:
       'Oversight for the research activity planning and execution, including mentorship external to the core team.',
   },
-  {
+  validation: {
     name: 'Validation',
     definition: 'Replication of results and other research outputs.',
   },
-  {
+  visualization: {
     name: 'Visualization',
     definition:
       'Preparation, creation and/or presentation of the published work, specifically data presentation.',
   },
-  {
+  writing: {
     name: 'Writing – original draft',
     definition:
       'Preparation, creation and/or presentation of the published work, specifically writing the initial draft.',
   },
-  {
+  writingReview: {
     name: 'Writing – review and editing',
     definition:
       'Critical review, commentary or revision – including pre- or post-publication stages.',
   },
-]
+}
