2984ea4b0536c4d1fb4b55d30477920f8a003fad
Andreas Bauer <dev@andreasbauer.org>
2024-07-10 09:31:16 +0200

    Add selection of different render styles

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,22 @@
 'use client'
 import { allCreditRoles, Credit, isCredit } from '../lib/credit/credit'
-import { toText } from '../lib/credit/generator-simple'
+import { toSimpleLatex } from '../lib/credit/generator-latex'
 import { CreditGenerator } from '../lib/credit/generator'
+import { useState } from 'react'
+import { toPlainText } from '@/lib/credit/generator-plaintext'
+
+const DEFAULT_STYLE = 'Plain Text'
+
+const availableStyles: { [key: string]: CreditGenerator } = {
+  [DEFAULT_STYLE]: toPlainText,
+  'LaTeX Simple': toSimpleLatex,
+}
 
 export default function Home() {
-  const generateCredit = (formData: FormData) => {
+  const [latexText, setLatexText] = useState('')
+  const [selectedStyle, setSelectedStyle] = useState(DEFAULT_STYLE)
+
+  const onFormAction = (formData: FormData) => {
     const authorName: string =
       formData.get('author-name')?.toString() || 'Author1'
 
@@ -15,9 +27,12 @@ export default function Home() {
       }
     })
 
-    const genFn: CreditGenerator = toText
+    const genFn: CreditGenerator = availableStyles[selectedStyle] || toPlainText
+    setLatexText(genFn(authorName, authorCredits))
+  }
 
-    console.log(genFn(authorName, authorCredits))
+  const onCopyHandler = () => {
+    console.log('TODO: imlement functionality')
   }
 
   return (
@@ -25,25 +40,13 @@ export default function Home() {
       <div className='w-fullconst flex flex-row gap-4'>
         {/* Left side */}
         <div className='w-1/2 divide-y divide-gray-200 overflow-hidden rounded-lg bg-white shadow'>
-          <div className='flex flex-wrap items-center justify-between px-4 py-5'>
+          <div className='flex-wggrap flex items-center justify-between px-4 py-5'>
             {/* Card header */}
             <label className='text-xl'>Author information</label>
-
-            <button
-              type='submit'
-              form='author-form'
-              className='relative inline-flex items-center rounded-md bg-indigo-500 px-2 py-1 text-sm font-semibold text-white shadow-sm hover:bg-indigo-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500'
-            >
-              Generate
-            </button>
           </div>
           <div className='p-5'>
             {/* Content goes here */}
-            <form
-              id='author-form'
-              className='space-y-4 '
-              action={generateCredit}
-            >
+            <form id='author-form' className='space-y-4 ' action={onFormAction}>
               <div className='rounded-md p-3 shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-indigo-600'>
                 <label
                   htmlFor='author-name'
@@ -95,11 +98,41 @@ export default function Home() {
 
         {/* Right side */}
         <div className='w-1/2 divide-y divide-gray-200 overflow-hidden rounded-lg bg-white shadow'>
-          <div className='px-4 py-5 '>
+          <div className='flex flex-wrap items-center justify-between px-4 py-4'>
             {/* Card header */}
-            <label className='text-xl'>Generated output</label>
+            <button
+              type='submit'
+              form='author-form'
+              className='relative inline-flex items-center rounded-md bg-indigo-500 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500'
+            >
+              Generate Output
+            </button>
+
+            <div>
+              <select
+                id='generator-style'
+                name='generator style'
+                onChange={(e) => setSelectedStyle(e.target.value)}
+                className='mt-2 block w-full rounded-md border-0 py-1.5 pl-3 pr-10 text-gray-900 ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-indigo-600 sm:text-sm sm:leading-6'
+              >
+                {Object.entries(availableStyles).map(([key, _]) => (
+                  <option key={key}>{key}</option>
+                ))}
+              </select>
+            </div>
+
+            <button
+              type='submit'
+              onClick={onCopyHandler}
+              className='rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50'
+            >
+              Copy to Clipboard
+            </button>
           </div>
-          <div className='px-4 py-5'>{/* Content goes here */}</div>
+          <div className='px-4 py-5'>
+            {/* Content goes here */}
+            {latexText}
+          </div>
         </div>
       </div>
     </main>
--- src/lib/credit/generator-plaintext.ts
+++ src/lib/credit/generator-plaintext.ts
@@ -1,11 +1,11 @@
 import { allCreditRoles, Credit } from './credit'
 
-export function toText(authorName: string, credits: Credit[]): string {
+export function toPlainText(authorName: string, credits: Credit[]): string {
   let allAuthorCredits = ''
   credits.forEach((creditKey) => {
     allAuthorCredits += ', ' + allCreditRoles[creditKey].name
   })
   allAuthorCredits = allAuthorCredits.slice(2)
 
-  return '\\textit{' + authorName + ':} ' + allAuthorCredits
+  return authorName + ': ' + allAuthorCredits
 }
