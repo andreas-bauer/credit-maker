e17c6f6172f621f117d8364dbd12b6f8cf7dfba1
Andreas Bauer <dev@andreasbauer.org>
2025-04-01 20:22:36 +0200

    Fix format

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,7 +2,7 @@ export function Footer() {
   return (
     <footer className='flex-none bg-white py-2'>
       <div className='flex items-center justify-end'>
-        <p className='z-40 mr-4 text-base text-gray-light'>
+        <p className='text-gray-light z-40 mr-4 text-base'>
           Copyright &copy; {new Date().getFullYear()} Andreas Bauer. All rights
           reserved.
         </p>
