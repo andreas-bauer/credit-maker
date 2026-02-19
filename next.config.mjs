f40738d26fd694bbed04912c38d1225b8d6d4883
Andreas Bauer <dev@andreasbauer.org>
2026-02-19 20:12:37 +0100

    Remove all NPM deps and React settings

--- next.config.mjs
+++
@@ -1,9 +0,0 @@
-const nextBasePath = process.env.NEXT_BASE_PATH || ''
-
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  output: 'export',
-  basePath: nextBasePath,
-}
-
-export default nextConfig
