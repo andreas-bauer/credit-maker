c05afd3428040f66beebc261753601eb823b194f
Andreas Bauer <dev@andreasbauer.org>
2026-03-01 22:43:14 +0100

    Add base path config for GH pages deploy

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,11 @@
 import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
 
+const basePath = process.env.BASE_PATH || ''
+
 export default defineConfig({
   root: 'src',
+  base: basePath,
   build: {
     outDir: '../dist',
   },
